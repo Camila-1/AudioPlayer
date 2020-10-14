1e17bfadfa64904c56f67e61e72bc6f33047c920
Camila-1 <camilaabalova@gmail.com>
2020-10-14 21:17:38 +0300

    play/pause button control, sending a broadcast from service to activity to handling the position of the seekbar

--- app/src/main/java/com/example/audioplayer/MainActivity.kt
+++ app/src/main/java/com/example/audioplayer/MainActivity.kt
@@ -1,34 +1,66 @@
 package com.example.audioplayer
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
-import androidx.appcompat.app.AppCompatActivity
+import android.content.*
 import android.os.Bundle
 import android.os.IBinder
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import android.view.View
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import com.example.audioplayer.application.AudioPlayerApplication
 import com.example.audioplayer.extensions.injectViewModel
 import com.example.audioplayer.services.AudioService
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        const val INTENT_ACTION = "MEDIA_PLAYER_ACTION"
+    }
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
 
     private var audioService: AudioService? = null
+    private var mediaController: MediaControllerCompat? = null
+    private var audioServiceBinder: AudioService.AudioServiceBinder? = null
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (!intent?.action.equals(INTENT_ACTION)) return
+            intent?.extras?.getInt("currentPosition")?.let {
+                current_time.text = TimeUnit.MILLISECONDS.toMinutes(it.toLong()).toString()
+                seek_bar.progress = it
+            }
+            intent?.extras?.getInt("duration")?.let {
+                duration.text = TimeUnit.MILLISECONDS.toMinutes(it.toLong()).toString()
+                seek_bar.max = it
+            }
+        }
+    }
 
     private var serviceConnection: ServiceConnection = object : ServiceConnection {
+
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            audioService = (service as AudioService.AudioServiceBinder).instance
+            audioServiceBinder = service as AudioService.AudioServiceBinder
+
+            mediaController = MediaControllerCompat(
+                this@MainActivity, audioServiceBinder!!.sessionToken
+            )
+            mediaController!!.registerCallback(object : MediaControllerCompat.Callback() {
+                override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+                    if (state?.state == PlaybackStateCompat.STATE_PLAYING)
+                        play_pause.setImageResource(R.drawable.ic_baseline_pause_circle_filled_24)
+                    else play_pause.setImageResource(R.drawable.ic_baseline_play_circle_filled_24)
+                }
+            })
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
-            audioService = null
+            audioServiceBinder = null
+            mediaController = null
         }
     }
 
@@ -50,30 +82,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             startService(intent)
             bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
         }
+    }
 
-        viewModel.mediaPlayerAction.observe(this, { playerAction ->
-            when(playerAction) {
-                is MediaPlayerAction.Prepare ->
-                    play_pause.setImageResource(R.drawable.ic_baseline_play_circle_filled_24)
-                is MediaPlayerAction.Pause -> {
-                    audioService?.pause()
-                    play_pause.setImageResource(R.drawable.ic_baseline_play_circle_filled_24)
-                }
-                is MediaPlayerAction.Play -> {
-                    audioService?.play()
-                    play_pause.setImageResource(R.drawable.ic_baseline_pause_circle_filled_24)
-                }
-                is MediaPlayerAction.Rewind -> audioService?.rewind(playerAction.seconds)
-                is MediaPlayerAction.Forward -> audioService?.forward(playerAction.seconds)
-            }
-        })
+    override fun onResume() {
+        super.onResume()
+        val intentFilter = IntentFilter().apply { addAction(INTENT_ACTION) }
+        registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(receiver)
     }
 
     override fun onClick(view: View?) {
         when(view) {
             prev_15 -> { viewModel.rewind() }
             next_15 -> { viewModel.forward() }
-            play_pause -> { viewModel.playOrPause() }
+            play_pause -> {
+                if (mediaController?.playbackState?.state == PlaybackStateCompat.STATE_PLAYING)
+                mediaController?.transportControls?.pause()
+                else mediaController?.transportControls?.play()
+            }
             else -> {  }
         }
     }
@@ -81,6 +111,5 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onDestroy() {
         super.onDestroy()
         unbindService(serviceConnection)
-        audioService?.stopSelf()
     }
 }
--- app/src/main/java/com/example/audioplayer/services/AudioService.kt
+++ app/src/main/java/com/example/audioplayer/services/AudioService.kt
@@ -1,9 +1,8 @@
 package com.example.audioplayer.services
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.Service
+import android.app.*
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.media.AudioAttributes
@@ -11,45 +10,88 @@ import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Binder
 import android.os.Build
+import android.os.Handler
 import android.os.IBinder
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import androidx.lifecycle.MutableLiveData
+import com.example.audioplayer.MainActivity
 import com.example.audioplayer.R
-import com.example.audioplayer.Track
 import com.example.audioplayer.application.AudioPlayerApplication
 import java.util.*
 
 
-class AudioService : Service() {
+class AudioService : Service(), Runnable {
 
     inner class AudioServiceBinder : Binder() {
         val instance: AudioService
-        get() = this@AudioService
-    }
-
-    fun seekBarHandle(track: MutableLiveData<Track>) {
-        Timer().schedule(object : TimerTask() {
-            override fun run() {
-                TODO("Not yet implemented")
-            }
-
-        }, 1000)
+            get() = this@AudioService
+        val sessionToken: MediaSessionCompat.Token
+        get() = mediaSession.sessionToken
     }
 
     private var notification: Notification? = null
-
     private lateinit var mediaPlayer: MediaPlayer
+    private val playbackStateBuilder = PlaybackStateCompat.Builder()
+    private lateinit var mediaSession: MediaSessionCompat
+    private val handler = Handler()
 
     override fun onCreate() {
         AudioPlayerApplication.appComponent.inject(this)
         super.onCreate()
+
+        mediaSession = MediaSessionCompat(this, "AudioService").apply {
+            setCallback(object : MediaSessionCompat.Callback() {
+                @RequiresApi(Build.VERSION_CODES.O)
+                override fun onPlay() {
+                    isActive = true
+                    setPlaybackState(
+                        playbackStateBuilder
+                            .setState(
+                                PlaybackStateCompat.STATE_PLAYING,
+                                PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN,
+                                1F
+                            ).build()
+                    )
+                    mediaPlayer.start()
+                    startForeground()
+                    handler.postDelayed(this@AudioService, 1000)
+                }
+
+                @RequiresApi(Build.VERSION_CODES.N)
+                override fun onPause() {
+                    mediaPlayer.pause()
+                    setPlaybackState(
+                        playbackStateBuilder.setState(
+                            PlaybackStateCompat.STATE_PAUSED,
+                            PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN,
+                            1F
+                        ).build()
+                    )
+                    stopForeground(STOP_FOREGROUND_REMOVE)
+                }
+
+                override fun onRewind() {
+                    super.onRewind()
+                }
+
+                override fun onFastForward() {
+                    super.onFastForward()
+                }
+            })
+
+            val activityIntent = Intent(applicationContext, MainActivity::class.java)
+            setSessionActivity(PendingIntent.getActivity(applicationContext, 0, activityIntent, 0))
+        }
+
+
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-        val uri = "https://www.dropbox.com/s/gbpkwex67ppmj7v/al_green_love_and_happiness_%28NaitiMP3.ru%29.mp3?dl=1"
+        val uri =
+            "https://www.dropbox.com/s/gbpkwex67ppmj7v/al_green_love_and_happiness_%28NaitiMP3.ru%29.mp3?dl=1"
 
         mediaPlayer = MediaPlayer().apply {
             setAudioAttributes(
@@ -61,54 +103,52 @@ class AudioService : Service() {
             setDataSource(applicationContext, Uri.parse(uri))
             prepare()
         }
-
-        notification = notification()
-
-        startForeground(1, notification)
-
         return START_NOT_STICKY
     }
 
-    fun play() {
-        mediaPlayer.start()
-    }
-
-    fun pause() {
-        mediaPlayer.pause()
-    }
-
-    fun rewind(seconds: Int) {
-
-    }
-
-    fun forward(seconds: Int) {
-
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun startForeground() {
+        notification = notification()
+        startForeground(1, notification)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun notification(): Notification? {
-            val chan = NotificationChannel(
-                "MyChannelId",
-                "My Foreground Service",
-                NotificationManager.IMPORTANCE_LOW
-            )
-            chan.lightColor = Color.BLUE
-            chan.lockscreenVisibility = Notification.VISIBILITY_SECRET
+        val chan = NotificationChannel(
+            "MyChannelId",
+            "My Foreground Service",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_SECRET
 
-            val manager = (getSystemService(NOTIFICATION_SERVICE) as NotificationManager)
-            manager.createNotificationChannel(chan)
+        val manager = (getSystemService(NOTIFICATION_SERVICE) as NotificationManager)
+        manager.createNotificationChannel(chan)
 
-            val builder = NotificationCompat.Builder(this, "Music")
-                .setOngoing(true)
-                .setSmallIcon(R.drawable.ic_baseline_play_circle_filled_24)
-                .setContentTitle("App is running on foreground")
-                .setPriority(NotificationManager.IMPORTANCE_LOW)
-                .setCategory(Notification.CATEGORY_SERVICE)
-                .setChannelId("MyChannelId")
+        val builder = NotificationCompat.Builder(this, "Music")
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_baseline_play_circle_filled_24)
+            .setContentTitle("App is running on foreground")
+            .setPriority(NotificationManager.IMPORTANCE_LOW)
+            .setCategory(Notification.CATEGORY_SERVICE)
+            .setChannelId("MyChannelId")
 
-            return builder.build()
+        return builder.build()
     }
 
     override fun onBind(intent: Intent?): IBinder? = AudioServiceBinder()
 
+    override fun onDestroy() {
+        super.onDestroy()
+        stopSelf()
+        mediaSession.release()
+    }
+
+    override fun run() {
+        val intent = Intent(MainActivity.INTENT_ACTION)
+            .putExtra("currentPosition", mediaPlayer.currentPosition)
+            .putExtra("duration", mediaPlayer.duration)
+        sendBroadcast(intent)
+        handler.postDelayed(this, 1000)
+    }
 }
